45513276742daa06dd752b38e20e3cf1cdd2ea71
zach <zachchou016@gmail.com>
2022-01-24 16:09:50 +0800

    complete variable test
    Signed-off-by: zach <zachchou016@gmail.com>

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -8,6 +8,10 @@ pub fn bigger(a: i32, b: i32) -> i32 {
     // - another function call
     // - additional variables
     // Execute `rustlings hint if1` for hints
+    if a > b{
+        return a;
+    }
+    return b;
 }
 
 // Don't mind this for now :)
